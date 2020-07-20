e7e9be704095d60ebd4e6dc590d0f0fe5c510557
Adam von Kraemer <adam.vk@live.se>
2020-07-20 11:24:15 +0200

    home page and file cleanup

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="page-container">
+            <div className="home-header-welcome">
+                Welcome to
+            </div>
+            <div className="home-header-tictactoe">
+                TicTacToe
+            </div>
+            <div className="home-description">
+                A fun game to play when having a few minutes over! Press the TicTacToe tab, choose your symbol, and get ready to beat that computer!
+            </div>
         </div>
     );
 };
